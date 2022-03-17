33bb16c5d970018d3a7ff54d68202b57947d2203
RafaelTDR <rafa.eel.tomazoni@gmail.com>
2022-03-17 17:02:37 -0300

    Desafio ML

--- Desafio_ML.py
+++ Desafio_ML.py
@@ -4,23 +4,33 @@
 
 n = 1000
 nm = 7770
-maxDigit = 6
+maxDigit = int (input())
 somaDigit = 21
-
+count = 0
 while n <= nm:
     
     numero = list(str(n))
     soma = 0
+    x=1
     for i in numero:
         
+        
+        
         if int(i) <= maxDigit:
-            
+                
             soma = soma + int(i)
             if soma == somaDigit:
-                print(n)
+                    
+                if x == 4:
+                    print(n)
+                    
+                    count = count + 1
         else:
             pass
-
+        x = x +1
     n = n + 1
+if count == 0:
+    print ("null")
+    
     
     
